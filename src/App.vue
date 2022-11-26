0a34f8d185a6e3624193d985ae64fbf722451a87
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-11-26 18:22:36 -0400

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+// import { RouterLink, RouterView } from 'vue-router'
+
+import Carrousel from "./components/Carrousel/Carrousel.vue"
+import Finder from "./components/Finder/Finder.vue"
+
+export default {
+    name: "app",
+    components: { Carrousel, Finder }
+}
+</script>
+
+<template>
+<h1 class="flex justify-center text-5xl">Periódico Digital</h1>
+
+<Carrousel />
+
+<Finder />
+</template>
