2b744b2c7fc32775bd96b1ea84defaf81a4fb35e
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-11-28 10:35:35 -0400

    Agregando input de búsqueda al componente buscador

--- src/components/Finder/Finder.vue
+++ src/components/Finder/Finder.vue
@@ -1,1 +1,10 @@
-<template></template>
+<template>
+    <section id="everything">
+      <div class="input-finder h-10 p-2 border border-black border-solid flex gap-2">
+          <input type="text" name="" id="" placeholder="Buscar" class="flex-auto">
+          <button class="flex-none" id="start-search">
+              <img class="h-3/4" src="https://cdn-icons-png.flaticon.com/512/3031/3031293.png" alt="Ícono de lupa">
+          </button>
+      </div>
+    </section>
+</template>
