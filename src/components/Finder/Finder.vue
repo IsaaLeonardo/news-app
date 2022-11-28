3cb72cd394d8a75b99a055a3a626940b6bcc6091
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-11-28 10:59:49 -0400

    Agregando contenedor de resultado de busqueda

--- src/components/Finder/Finder.vue
+++ src/components/Finder/Finder.vue
@@ -1,10 +1,15 @@
 <template>
-    <section id="everything">
-      <div class="input-finder h-10 p-2 border border-black border-solid flex gap-2">
+    <section id="everything" class="w-full max-w-2xl mt-3 flex flex-col items-center gap-3">
+      <div class="input-finder w-80 h-10 p-2 border border-black border-solid flex gap-2">
           <input type="text" name="" id="" placeholder="Buscar" class="flex-auto">
           <button class="flex-none" id="start-search">
               <img class="h-3/4" src="https://cdn-icons-png.flaticon.com/512/3031/3031293.png" alt="Ícono de lupa">
           </button>
+      </div>
+
+      <!-- El height debe ser cambiado por un valor relativo cuando empiece a cargar noticias -->
+      <div class="results w-full h-96 border border-black border-solid">
+
       </div>
     </section>
 </template>
