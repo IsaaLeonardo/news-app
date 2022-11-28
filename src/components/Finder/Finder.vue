22c46d2f56efd4145e36e254a2388b3541afae0d
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2022-11-28 17:42:46 -0400

    Mostrando resultados al usuario de búsqueda

--- src/components/Finder/Finder.vue
+++ src/components/Finder/Finder.vue
@@ -5,29 +5,30 @@ export default {
     name: 'finder',
     data() {
         return {
-            search: ""
+            search: "",
+            data: []
         }
     },
     methods: {
-        searchData() {
+        async getData() {
             const apikey = "03688902f4484c859e95e2fdae559147"
             const input = this.search.replaceAll(" ", "+")
             const URL = 'https://newsapi.org/v2/everything?' +
                         'q=' + input + '&' +
+                        'language=' + 'es' + '&' +
+                        `searchIn=` + 'title' + '&' +
+                        'sortBy=' + 'popularity' + '&' +
                         'apiKey=' + apikey
 
             axios.get(URL)
                 .then(response =>{
-                    const data = response.data
-
-                    console.log('All ok')
-                    console.log(data)
+                    this.data = response.data
+                    console.log('Se realizó la petición a:')
                     console.log(URL)
-
+                    console.log(this.data)
                 })
                 .catch(error => {
-                    console.log('All its not ok')
-                    console.log(URL)
+                    console.log(error)
                 })
         }
     },
@@ -44,12 +45,12 @@ export default {
             placeholder="Buscar"
             class="flex-auto"
             v-model.trim="search"
-            @keyup.enter="searchData"
+            @keyup.enter="getData"
         />
         <button
             class="flex-none"
             id="start-search"
-            @click="searchData"
+            @click="getData"
         >
             <img class="h-3/4" src="https://cdn-icons-png.flaticon.com/512/3031/3031293.png" alt="Ícono de lupa">
         </button>
@@ -67,7 +68,7 @@ export default {
             class="w-1/6"
             >
             <div class="result-info">
-                <h2 class="title text-xl mb-1">España Ganó</h2>
+                <h2 class="title text-xl mb-1">{{data.articles}}</h2>
                 <div class="owner flex">
                     <p class="source">Google news</p>
                     <span>&nbsp;-&nbsp;</span>
